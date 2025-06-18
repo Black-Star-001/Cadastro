c51db7fef6a435c35efd48cc5ea9aebf7ca51375
Guilherme <>
2025-06-18 07:49:08 -0300

    Comit inicial do projeto

---
+++ src/main/java/mysql/mavenproject3/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mysql.mavenproject3;
+
+/**
+ *
+ * @author guilh
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
